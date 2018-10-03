c1d894d292c9158479f90b755739cd0fb67554c9
hhqy <huhuan@corp.netease.com>
2018-10-04 00:06:15 +0800

    add a new file

---
+++ File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  HairStyle
+//
+//  Created by 胡欢 on 2018/10/4.
+//  Copyright © 2018 Lawrenauh. All rights reserved.
+//
+
+import Foundation
+
+
+print("we need something else")
